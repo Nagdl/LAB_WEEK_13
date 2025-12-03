b73de6b56f372f498cf3989d4a9f4fb0920373bc
Nagdl <137997774+Nagdl@users.noreply.github.com>
2025-12-03 15:59:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_lab_week_13/MovieViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.test_lab_week_13
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+
+
+
+class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+
+    private val _popularMovies = MutableStateFlow(
+        emptyList<Movie>()
+    )
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
+    init {
+        fetchPopularMovies()
+    }
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies().catch {
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect { movies ->
+                val currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString()
+
+                val filtered = movies
+                    .filter { movie ->
+                        movie.releaseDate?.startsWith(currentYear) == true
+                    }
+                    .sortedByDescending { movie ->
+                        movie.popularity
+                    }
+
+                _popularMovies.value = filtered
+            }
+        }
+    }
+}
